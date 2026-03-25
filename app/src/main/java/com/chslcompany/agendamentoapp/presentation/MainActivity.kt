435fde56a0a962a1f3cb2c95c03b652085a0f4e3
Carlos Lima <inv.clima@ifood3rd.com>
2026-03-25 14:41:13 -0300

    deixando splash do sistema que é exibida a partir do android 12 apenas com fundo cinza sem icone

--- app/src/main/java/com/chslcompany/agendamentoapp/presentation/MainActivity.kt
+++ app/src/main/java/com/chslcompany/agendamentoapp/presentation/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.chslcompany.agendamentoapp.presentation.navigation.AgendaNavHost
 import com.chslcompany.agendamentoapp.presentation.theme.AgendamentoAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
